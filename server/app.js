e6cbe19c0e077562ceb990a9101432c862dbd90e
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2021-09-27 08:21:39 +0100

    added a category resolver

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
-const {mainCards, animals}  = require('./db')
+const { mainCards, animals, categories } = require("./db");
 
 const typeDefs = gql`
   type MainCard {
@@ -8,27 +8,66 @@ const typeDefs = gql`
   }
 
   type Animal {
+    id: ID!
     title: String!
     image: String!
     rating: Float
     pricing: String!
     description: [String!]!
     stock: Int!
+    slug: String!
     onSale: Boolean
+    category: Category
+  }
+
+  type Category {
+    id: ID!
+    image: String!
+    category: String!
+    slug: String!
+    animals: [Animal!]!
   }
 
   type Query {
     mainCards: [MainCard]
     animals: [Animal]
+    animal(slug: String): Animal
+    categories: [Category!]!
+    category(slug: String): Category!
   }
 `;
-
-
 
 const resolvers = {
   Query: {
     mainCards: () => mainCards,
     animals: () => animals,
+    animal: (parent, args, ctc) => {
+      let animal = animals.find((animal) => {
+        return animal.slug === args.slug;
+      });
+      return animal;
+    },
+    categories: () => categories,
+    category: (parent, args, ctx) => {
+      let category = categories.find((category) => {
+        return category.slug === args.slug;
+      });
+
+      return category;
+    },
+  },
+  Category: {
+    animals: (parent, args, ctx) => {
+      return animals.filter((animal) => {
+        return animal.category === parent.id;
+      });
+    },
+  },
+
+  Animal: {
+    category: (parent, args, ctx) => {
+      console.log(parent);
+    },
   },
 };
 
